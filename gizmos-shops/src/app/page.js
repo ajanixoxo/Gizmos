7c38067ea7cecf73f140fd36e6bb1bf3566e8abc
Adeoluwa Gbenro <joelayomide35@gmail.com>
2024-08-17 03:10:14 +0100

    Gizmos

---
+++ gizmos-shops/src/app/page.js
@@ -0,0 +1,13 @@
+"use client"
+import Image from "next/image";
+import Navbar from "./components/Navbar/Navbar"
+import {useState} from "react"
+
+export default function Home() {
+  
+  return (
+  <main>
+   <Navbar/>
+  </main>
+  );
+}
